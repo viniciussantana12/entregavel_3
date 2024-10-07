197bb2f312d9099347cca1a25a0845b0aa442de3
viniciuss1203@outlook.com <viniciuss1203@outlook.com>
2024-10-07 15:19:21 -0300

    update

--- todoList/js/scripts.js
+++ todoList/js/scripts.js
@@ -1,7 +1,13 @@
 // Clase
 
 class ToDo {
-
+  Texto 
+  Prioridade 
+  Feito = false
+  constructor(texto, prioridade){
+    this.Texto = texto;
+    this.Prioridade = prioridade;
+  }
 }
 
 // Array
@@ -9,31 +15,70 @@ class ToDo {
 
 //funções projeto
 
-function CriarToDo() {
-
+function CriarToDo(texto, prioridade, array) {
+  let objetoTodo = new ToDo(texto, prioridade)
+  if(!array.some(x => x.Texto == texto))
+    array.push(objetoTodo)
+  return objetoTodo
 }
 
-function AtualizarToDo() {
-
+function AtualizarToDo(textoAntigo, textoNovo, array) {
+  let atualizado = false 
+  array.forEach(todo => {
+    if(todo.Texto == textoAntigo) {
+      todo.Texto = textoNovo
+      atualizado = true 
+    }
+  })
+  return atualizado 
 }
 
-function ConcluirToDo() {
-
+function ConcluirToDo(array, texto) {
+  let concluido = false 
+  array.forEach(todo => {
+    if(todo.Texto == texto) {
+      if(todo.Feito)
+        todo.Feito = false 
+      else {
+        todo.Feito = true
+      }
+      concluido = true 
+    }
+  })
+  return concluido
 }
 
-function ExcluirToDo() {
-
+function ExcluirToDo(array, texto) {
+  let index
+  let removido = false
+  array.forEach(todo => {
+    if(todo.Texto == texto) {
+      index = array.indexOf(todo)
+      removido = true
+    }
+  })
+  array.splice(index, 1)
+  return removido 
 }
 
-function PesquisarToDo() {
- 
+function PesquisarToDo(array, texto) {
+  let pesquisa = false
+  array.forEach(todo => {
+    if (todo.Texto.includes(texto)) {
+      pesquisa = true 
+    }
+  })
+
+  return pesquisa 
 }
 
-function OrdenarCrescente() {
-  
+function OrdenarCrescente(array) {
+  array.sort((a, b) => a.Prioridade - b.Prioridade)
+  return array
 }
-function OrdenarDecrescente() {
-  
+function OrdenarDecrescente(array) {
+  array.sort((a, b) => b.Prioridade - a.Prioridade)
+  return array
 }
 
 // Seleção de elementos
